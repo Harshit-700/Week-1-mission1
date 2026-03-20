d1cfa302e486e7fc069edf5ec70286b5c1ca5262
Harshit-700 <149258756+Harshit-700@users.noreply.github.com>
2026-03-20 14:27:47 +0530

    Add files via upload

---
+++ Mission1/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
+
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
+});
